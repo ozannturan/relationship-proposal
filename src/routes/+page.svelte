dc34c89d7b8f8a13ea580c3edd2203d76d5a403c
Ozan Turan <ozann.trn@gmail.com>
2024-02-21 14:28:39 +0300

    ali-hasan

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,2 +1,15 @@
-<h1>Ali Hasan Seni Seviyorum</h1>
-<p>Visit <a href="https://kit.svelte.dev">kit.svelte.dev</a> to read the documentation</p>
+<script>
+	function yes() {
+		alert('tebrikler')
+	}
+
+	function no() {
+		alert('Allah Kahretsin seni')
+	}
+</script>
+
+<div class="flex flex-col items-center justify-center h-screen bg-slate-500 text-6xl">
+	<h1 class="bg-yellow-500">Benimle çıkar mısın?</h1>
+	<button on:click={yes} class="bg-green-500 hover:scale-150 duration-200">evet</button>
+	<button on:click={no} class="bg-red-950 hover:scale-50 hover:rotate-90">hayır</button>
+</div>
