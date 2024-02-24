314289e4b165d93a434e6aedd807322702b2d6e5
ozannturan <160431720+ozannturan@users.noreply.github.com>
2024-02-24 22:06:17 +0300

    Update +page.svelte

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,15 +1,15 @@
 <script>
 	function yes() {
-		alert('tebrikler')
+		alert('Congrulations!')
 	}
 
 	function no() {
-		alert('Allah Kahretsin seni')
+		alert('U Lost Me')
 	}
 </script>
 
 <div class="flex flex-col items-center justify-center h-screen bg-slate-500 text-6xl">
-	<h1 class="bg-yellow-500">Benimle çıkar mısın?</h1>
-	<button on:click={yes} class="bg-green-500 hover:scale-150 duration-200">evet</button>
-	<button on:click={no} class="bg-red-950 hover:scale-50 hover:rotate-90">hayır</button>
+	<h1 class="bg-yellow-500">Will you go on a date with me?</h1>
+	<button on:click={yes} class="bg-green-500 hover:scale-150 duration-200">Yes</button>
+	<button on:click={no} class="bg-red-950 hover:scale-50 hover:rotate-90">No</button>
 </div>
